f3cbc8b2330664c9c0b917c4fb82d500a605de5c
leelk <karunarathne1995@gmail.com>
2023-03-29 17:48:48 +0530

    Add Security for API endPoints

---
+++ src/main/java/com/hilltop/configuration/utill/JwtUtil.java
@@ -0,0 +1,26 @@
+package com.hilltop.configuration.utill;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+
+@Component
+public class JwtUtil {
+
+    public static final String SECRET = "645267556B58703273357638792F423F4528472B4B6250655368566D59713374645267556B58703273357638792F423F4528472B4B6250655368566D59713374";
+
+
+    public void validateToken(final String token) {
+        Jwts.parserBuilder().setSigningKey(getSignKey()).build().parseClaimsJws(token);
+    }
+
+
+
+    private Key getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
